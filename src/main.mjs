14f66fb6e0c6ba0b07f94d7f003920fc23eaa2ef
Nick K <nick.kruchinin@gmail.com>
2019-01-16 12:11:06 +0300

    Expose FileSizeLimitError.

--- src/main.mjs
+++ src/main.mjs
@@ -1,6 +1,7 @@
 export {
   default as RequestEntityTooLargeError
 } from "lib/error/RequestEntityTooLargeError"
+export {default as FileSizeLimitError} from "lib/error/FileSizeLimitError"
 export {default as FieldsLimitError} from "lib/error/FieldsLimitError"
 export {default as FilesLimitError} from "lib/error/FilesLimitError"
 export {default as PartsLimitError} from "lib/error/PartsLimitError"
