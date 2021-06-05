1b16fdac4bf3956087b5d8c53533ed18db4754a3
Nick K <nick.kruchinin@gmail.com>
2021-06-05 13:27:55 +0300

    Add one more test for main function.

--- lib/parse.test.ts
+++ lib/parse.test.ts
@@ -1,6 +1,8 @@
+import {promises as fs} from "fs"
+
 import test from "ava"
 
-import {FormData, File} from "formdata-node"
+import {FormData, File, fileFromPath} from "formdata-node"
 
 import createRequest from "./__helper__/createRequest"
 import createServer from "./__helper__/createServer"
@@ -70,14 +72,30 @@ test("Ignores types casting when options.castTypes set to false", async t => {
   t.deepEqual(body, expected)
 })
 
-test("Parses a File", async t => {
+test("Parses form with a File", async t => {
   const expected = "My hovercraft is full of eels"
 
   const form = new FormData()
 
   form.set("file", new File([expected], "file.txt"))
 
-  const res = await createRequest(createServer(parse), form)
+  const {body} = await createRequest(createServer(parse), form)
 
-  t.deepEqual(res.body, {file: expected})
+  t.deepEqual(body, {file: expected})
+})
+
+test("Parses form with both files and fields", async t => {
+  const expected = {
+    field: "Field content",
+    file: await fs.readFile("license", "utf-8")
+  }
+
+  const form = new FormData()
+
+  form.set("field", expected.field)
+  form.set("file", await fileFromPath("license"))
+
+  const {body} = await createRequest(createServer(parse), form)
+
+  t.deepEqual(body, expected)
 })
