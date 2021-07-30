402e720442746db5eadb50836c0808dc0eb635a0
Nick K <nick.kruchinin@gmail.com>
2021-07-30 17:55:13 +0300

    Move env variables into ava.config.cjs

--- ava.config.cjs
+++ ava.config.cjs
@@ -1,5 +1,9 @@
 module.exports = {
+  environmentVariables: {
+    TS_NODE_COMPILER: "ttypescript",
+    TS_NODE_PROJECT: "tsconfig.cjs.json"
+  },
   extensions: ["ts"],
-  require: ["dotenv/config", "ts-node/register"],
+  require: ["ts-node/register/transpile-only"],
   files: ["lib/**/*.test.ts"],
 }
