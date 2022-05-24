7dc38e6c18564d64c97a8f493653f1271e97c546
Nick K <nick.kruchinin@gmail.com>
2022-05-24 18:37:46 +0300

    Remove ttypecript from ava.config.cjs

--- ava.config.cjs
+++ ava.config.cjs
@@ -1,9 +1,14 @@
 module.exports = {
   environmentVariables: {
-    TS_NODE_COMPILER: "ttypescript",
     TS_NODE_PROJECT: "tsconfig.cjs.json"
   },
-  extensions: ["ts"],
-  require: ["ts-node/register/transpile-only"],
-  files: ["lib/**/*.test.ts"],
+  extensions: [
+    "ts"
+  ],
+  require: [
+    "ts-node/register/transpile-only"
+  ],
+  files: [
+    "lib/**/*.test.ts"
+  ],
 }
